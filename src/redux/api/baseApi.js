6ae299d99c3107fd21d532532abdfb899fabb9e4
safayet nur <safayetnurelectra@gmail.com>
2025-12-03 16:46:25 +0600

    screenshot added

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1",
+    // baseUrl: "http://localhost:8080/api/v1",
+    baseUrl: "https://dentists-portal-backend.vercel.app/api/v1",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
